49e76d57a8d754349941c284a86fd47859fea134
yash0412 <yashagar0412@gmail.com>
2020-05-23 15:04:52 +0530

    Phone login with otp and google login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { LoginPageRoutingModule } from './login-routing.module';
-
-import { LoginPage } from './login.page';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FirebaseAuthentication } from "@ionic-native/firebase-authentication/ngx";
+import { IonicModule } from "@ionic/angular";
+import { LoginPageRoutingModule } from "./login-routing.module";
+import { LoginPage } from "./login.page";
+import { GooglePlus } from "@ionic-native/google-plus/ngx";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  providers: [FirebaseAuthentication, GooglePlus]
 })
 export class LoginPageModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FirebaseAuthentication } from "@ionic-native/firebase-authentication/ngx";
+import { GooglePlus } from "@ionic-native/google-plus/ngx";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
+  phoneNumberForm: FormGroup;
+  otpForm: FormGroup;
+  showOtpInput: boolean = false;
+  verficationId: string;
+  constructor(
+    private fb: FormBuilder,
+    private firebaseAuthentication: FirebaseAuthentication,
+    private googlePlus: GooglePlus
+  ) {
+    this.phoneNumberForm = this.fb.group({
+      phoneNumber: [
+        "",
+        [Validators.required, Validators.pattern(/^[1-9][0-9]{9}$/)]
+      ]
+    });
+    this.otpForm = this.fb.group({
+      otp: ["", [Validators.required, Validators.pattern(/[0-9]{6}/)]]
+    });
 
-  constructor() { }
-
-  ngOnInit() {
+    this.firebaseAuthentication.onAuthStateChanged().subscribe(
+      data => {
+        console.log(data);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
+  sendOtp() {
+    this.showOtpInput = true;
+    this.firebaseAuthentication
+      .verifyPhoneNumber("+91" + this.phoneNumberForm.value.phoneNumber, 120)
+      .then(data => {
+        this.verficationId = data;
+      });
+  }
+
+  verifyOtp() {
+    this.firebaseAuthentication
+      .signInWithVerificationId(this.verficationId, this.otpForm.value.otp)
+      .then(data => {
+        console.log(data);
+      });
+  }
+
+  signInWithGoogle() {
+    this.googlePlus
+      .login({})
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
+
+  ngOnInit() {}
 }
