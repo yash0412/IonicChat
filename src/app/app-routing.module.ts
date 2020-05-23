5dbb5f89299d7184511435394edb40f5bbc58851
yash0412 <yashagar0412@gmail.com>
2020-05-23 15:24:51 +0530

    Home page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: "login",
-    loadChildren: () =>
-      import("./login/login.module").then(m => m.LoginPageModule)
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
   },
   {
     path: "home",
     loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
-  },
-  {
-    path: "",
-    redirectTo: "login",
-    pathMatch: "full"
   },
   {
     path: "login",
